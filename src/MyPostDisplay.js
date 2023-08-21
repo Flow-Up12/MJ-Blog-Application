afe0fac41d67be377ed74b72e8f02fc3f7722ddd
Flow-Up12 <marcosje2005@icloud.com>
2023-08-20 22:32:12 -0500

    fix css

--- src/MyPostDisplay.js
+++ src/MyPostDisplay.js
@@ -3,60 +3,66 @@ import { Avatar, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import MyPostFeed from './MyPostFeed';
 
-const MyPostDisplay = ({ myPost, usernameInput, userImg, name, bio, setEditingProfile}) => {
+
+const MyPostDisplay = ({ myPost, usernameInput, userImg, name, bio, setEditingProfile }) => {
   const [activeTab, setActiveTab] = useState('post'); // Default active tab is "post"
   const name2 = usernameInput;
   const handleTabChange = (tab) => {
     setActiveTab(tab);
   };
-  
+
   return (
     <main className="Home">
-    <div style={{borderBottom: '1px solid black'}}>
-      <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            fontSize: 30,
-          }}
-        >
-          <Avatar src={userImg} sx={{ width: 60, height: 60 }} alt="Profile Img" style={{ marginRight: 20 }} />
-            
-            {usernameInput}
+      <div className="MyPostDisplay"> 
+      <div className='username'>{usernameInput}</div>  
+        <div className="MyPostDisplay-header">
+          <Avatar
+            sx={{ width: 70, height: 70 }}
+            src={userImg}
+            alt="Profile Img"
+            className="MyPostDisplay-header-avatar"
+         
+          />
+         
 
-            <div style={{ marginBottom: 20, marginLeft:40, marginTop:24 }}>
+          <div style={{ marginBottom: 20, marginTop:24 }}>
             <Button color="primary"
               onClick={() => handleTabChange('post')}
-              style={{ fontSize:15,marginRight: 10, color: activeTab === 'post' ? 'black' : 'lightgray', backgroundColor: 'transparent'}}
+              className="MyPostDisplay-button"
+              style={{fontSize:15, marginRight: 10, color: activeTab === 'post' ? 'black' : 'lightgray', backgroundColor: 'transparent'}}
             >
               Posts
             </Button>
             <Button color="primary"
               onClick={() => handleTabChange('followers')}
+              className="MyPostDisplay-button"
               style={{fontSize:15, marginRight: 10, color: activeTab === 'followers' ? 'black' : 'lightgray', backgroundColor: 'transparent'}}
             >
               Followers
             </Button>
             <Button color="primary" 
               onClick={() => handleTabChange('following')}
+              className="MyPostDisplay-button"
               style={{ fontSize:15, marginRight: 10, color: activeTab === 'following' ? 'black' : 'lightgray', backgroundColor: 'transparent'}}
             >
               Following
             </Button>
           </div>
         </div>
-        <div style= {{display:'flex', flexDirection: 'column'}}>
-          <div>{!name.length ? name2 : name} </div>
-          <div style={{ marginTop: 10}} >{bio}</div>
+        <div className="MyPostDisplay-content"> {/* Apply CSS class */}
+          <div className="MyPostDisplay-name">{!name.length ? name2 : name}</div> {/* Apply CSS class */}
+          <div className="MyPostDisplay-bio">{bio}</div> {/* Apply CSS class */}
         </div>
-        <Button 
-          style={{ marginTop: 30, marginBottom: 20, marginRight: 10, backgroundColor: 'lightgray', color:'black', }}
-         > <Link onClick={setEditingProfile} to="editProfile" style={{color: 'black', textDecoration:'none'}}>Edit Profile</Link>
+        <Button
+           style={{ marginTop: 30, marginBottom: 20, marginRight: 10, backgroundColor: 'lightgray', color:'black', }}
+        >
+          <Link onClick={setEditingProfile} to="editProfile" style={{ color: 'black', textDecoration: 'none' }}>
+            Edit Profile
+          </Link>
         </Button>
-    </div>
-      
+      </div>
 
-         {activeTab === 'post' ? (
+      {activeTab === 'post' ? (
         myPost.length ? <MyPostFeed myPost={myPost} /> : <p style={{ marginTop: '2rem' }}>No posts to display.</p>
       ) : (
         <p>Content for {activeTab} tab goes here.</p>
