8797b81ff7f12038a2a5281fe65117f5ffcb9dc3
Flow-Up12 <marcosje2005@icloud.com>
2023-08-21 22:19:04 -0500

    updated react-dom version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+
+const app = (
+  <Router>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </Router>
 );
 
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(app);
+} else {
+  console.error("Root element 'root' not found in the document.");
+}
